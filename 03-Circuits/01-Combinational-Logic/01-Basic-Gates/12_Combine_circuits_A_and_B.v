4625cc31404ba27bf9c27c16d4360b57ae4fc804
Prem Arun <premarun375@gmail.com>
2026-04-10 21:24:36 +0530

    Added new file

---
+++ 03-Circuits/01-Combinational-Logic/01-Basic-Gates/12_Combine_circuits_A_and_B.v
@@ -0,0 +1,10 @@
+module top_module (input x, input y, output z);
+    wire z1,z2,z3,z4,z5,z6;
+    assign z1 = (x ^ y) & x;
+    assign z2 = ((~x & ~y) | (x & y));
+    assign z3 = (x ^ y) & x;
+    assign z4 = ((~x & ~y) | (x & y));
+    assign z5 = (z1 | z2);
+    assign z6=  (z3 & z4);    
+    assign z = (z5 ^ z6);
+endmodule
