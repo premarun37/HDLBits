14226ad8c9a531ba93f5497d0417b6fef25d73dd
Prem Arun <premarun375@gmail.com>
2026-05-04 04:53:16 +0530

    Added new file

---
+++ 03-Circuits/01-Combinational-Logic/03-Arithmetic-Circuits/03_3_bit_binary_adder.v
@@ -0,0 +1,40 @@
+module top_module( 
+    input [2:0] a, b,
+    input cin,
+    output [2:0] cout,
+    output [2:0] sum );
+	
+    fa fa0 (
+        .a(a[0]),
+        .b(b[0]),
+        .cin(cin),
+        .cout(cout[0]),
+        .sum(sum[0])
+    );
+    
+     fa fa1 (
+         .a(a[1]),
+         .b(b[1]),
+         .cin(cout[0]),
+         .cout(cout[1]),
+         .sum(sum[1])
+    );
+    
+     fa fa2 (
+         .a(a[2]),
+         .b(b[2]),
+         .cin(cout[1]),
+         .cout(cout[2]),
+         .sum(sum[2])
+    );
+      	
+endmodule
+
+module fa( 
+    input a, b, cin,
+    output cout, sum );
+
+    assign sum = a ^ b ^cin;
+    assign cout = (a & b) | (a & cin) | (b & cin);
+endmodule
+
