fa71310fb5a36913855a28ad5564e52759c81c7e
Prem Arun <premarun375@gmail.com>
2026-07-24 02:03:14 +0530

    Added a new file

---
+++ 03-Circuits/02-Sequential-Logic/01-Latches-and-Flip-Flops/12_MUX_and_DFF.v
@@ -0,0 +1,22 @@
+module top_module (
+    input clk,
+    input w, R, E, L,
+    output Q
+);
+
+    always @(posedge clk) begin
+      
+      if (L) begin
+          Q <= R;
+      end
+            
+        else begin
+             if (E) begin
+        		Q <=w;
+             end
+            else begin
+            	Q <= Q;
+            end
+      end
+    end
+endmodule
